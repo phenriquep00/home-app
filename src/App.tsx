5a4872e437891c2e8b8fcb98bcf3753efe096c3e
Pedro Henrique <contact.pedrolima@gmail.com>
2023-03-02 11:07:21 -0300

    git setup

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="App">
+      <h1>Site da casa</h1>
+      <h2>Vo faze ainda kk</h2>
+    </div>
+  )
+}
+
+export default App
